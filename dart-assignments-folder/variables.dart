1f08b795e74ad0f9e2cca380afac1f35c5a167d4
Iamouma <stephenouma452@gmail.com>
2024-09-11 09:35:44 +0300

    A Dart program that defines and initializes variables of types int, double, string, bool and list

---
+++ dart-assignments-folder/variables.dart
@@ -0,0 +1,29 @@
+// Main function: Entry point of the Dart program
+void main() {
+  // 1. Integer (int) variable
+  // Used to store whole numbers
+  int age = 25;
+  
+  // 2. Double (double) variable
+  // Used to store floating-point numbers (numbers with decimals)
+  double height = 5.9;
+  
+  // 3. String (String) variable
+  // Used to store a sequence of characters (text)
+  String name = "Alice";
+
+  // 4. Boolean (bool) variable
+  // Used to store true or false values
+  bool isStudent = true;
+
+  // 5. List (List) variable
+  // Used to store a collection of items, can be of any data type
+  List<String> hobbies = ["Reading", "Traveling", "Coding"];
+
+  // Printing the variables to the console
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Is Student: $isStudent");
+  print("Hobbies: $hobbies");
+}
